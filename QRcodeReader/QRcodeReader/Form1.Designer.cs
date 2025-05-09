9f040e819b725ed7b157d945f0e7025bd96e25e1
Elias <136005060+Edavid18@users.noreply.github.com>
2025-05-09 10:16:28 -0500

    Add files via upload

---
+++ QRcodeReader/QRcodeReader/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace QRcodeReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(554, 299);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 314);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(37, 359);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(261, 359);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Location = new Point(647, 12);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(550, 665);
+            webView21.TabIndex = 4;
+            webView21.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1247, 689);
+            Controls.Add(webView21);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
---
+++ QRcodeReader/QRcodeReader/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using ZXing;
+using ZXing.QrCode;
+using System.Text.RegularExpressions;
+using AForge.Video.DirectShow;
+using System.Linq.Expressions;
+using Microsoft.Web.WebView2.Core;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ZXing.Windows.Compatibility;
+
+
+namespace QRcodeReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Agregamos referencias
+        private FilterInfoCollection Dispositivos;
+        private VideoCaptureDevice Camara;
+        public static bool ValidateUrl(string url) // METODO CON EXPRESION REGULAR QUE VALIDA LAS URL
+        {
+            if (url == null || url == "") return false;
+            Regex oRegExp = new Regex(@"(http|ftp|https)://([\w-]+\.)+(/[\w- ./?%&=]*)?", RegexOptions.IgnoreCase);
+
+            return oRegExp.Match(url).Success;
+        }
+        private void Camara_NewFrame(object sender, NewFrameEventArgs
+       eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i = 0; // Contadora
+                       // Listamos dispositivos
+            Dispositivos = new
+           FilterInfoCollection(FilterCategory.VideoInputDevice);
+            //Cargamos dispositivos al combobox
+            foreach (FilterInfo Device in Dispositivos)
+            {
+                //Contadora de camaras
+                i = i + 1;
+                comboBox1.Items.Add(Device.Name);
+            }
+            comboBox1.SelectedIndex = -1;
+            if (i == 0) // Si no hay camara deshabilitar el boton
+            {
+                button1.Enabled = false;
+
+            }
+            else // Si hay camara el boton se habilita
+            {
+                button1.Enabled = true;
+            }
+            // Iniciar control de video
+            Camara = new VideoCaptureDevice();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("No hay ningún dispositivo seleccionado.");
+                return;
+            }
+            //Establecemos el dispositivo seleccionado como fuente de video
+            Camara = new
+            VideoCaptureDevice(Dispositivos[comboBox1.SelectedIndex].MonikerString);
+            Camara.NewFrame += new
+            NewFrameEventHandler(Camara_NewFrame);
+            //Iniciar recepcion de video
+            Camara.Start();
+            // Habilitamos y empezamos el timer
+            timer1.Enabled = true;
+            timer1.Start();
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap image = (Bitmap)pictureBox1.Image.Clone();
+                image.RotateFlip(RotateFlipType.Rotate180FlipX);
+
+                BarcodeReader Reader = new BarcodeReader();
+                Result result = Reader.Decode(image);
+                image.Dispose(); // Dispose of the cloned image
+
+                try
+                {
+                    if (result != null)
+                    {
+                        string decoded = result.ToString();
+
+                        if (!string.IsNullOrEmpty(decoded))
+                        {
+                            timer1.Stop(); // Stop the timer
+
+                            // ✅ Stop and fully release the camera
+                            if (Camara != null)
+                            {
+                                if (Camara.IsRunning)
+                                {
+                                    Camara.SignalToStop();
+                                    Camara.WaitForStop();
+                                }
+
+                                Camara.NewFrame -= Camara_NewFrame; // 🛑 Detach event handler
+                                Camara = null;
+                            }
+
+                            // ✅ Dispose of the PictureBox image
+                            if (pictureBox1.Image != null)
+                            {
+                                pictureBox1.Image.Dispose();
+                                pictureBox1.Image = null;
+                            }
+
+                            if (!ValidateUrl(decoded))
+                            {
+                                MessageBox.Show("El código QR no corresponde a una URL, pero contiene: " + decoded);
+                            }
+                            else
+                            {
+                                webView21.Source = new Uri(decoded);
+                                MessageBox.Show("Navegando a " + decoded);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al procesar el código QR: " + ex.Message);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (timer1.Enabled)
+                {
+                    timer1.Stop();
+                    timer1.Dispose();
+                }
+
+                // ✅ Fully shut down the camera
+                if (Camara != null)
+                {
+                    if (Camara.IsRunning)
+                    {
+                        Camara.SignalToStop();
+                        Camara.WaitForStop();
+                    }
+
+                    Camara.NewFrame -= Camara_NewFrame; // Detach event handler
+                    Camara = null;
+                }
+
+
+                if (pictureBox1.Image != null)
+                {
+                    pictureBox1.Image.Dispose();
+                    pictureBox1.Image = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cerrar: " + ex.Message);
+            }
+
+            Application.Exit();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
